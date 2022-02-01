9bb5db3bfae4b7bfde5d8ffcc37b54370cd50b50
nileshgavali407 <72439267+nileshgavali407@users.noreply.github.com>
2022-02-01 12:16:28 +0530

    day1 assignment submitted

---
+++ day1assignment.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = int(input('Enter the no: '))
+b = int(input('Enter the  no: '))
+c = int(input('Enter the no: '))
+d = int(input('Enter the no: '))
+f = int(input('Enter the no: '))
+g = int(input('Enter the no: '))
+h = int(input('Enter the no: '))
+i = int(input('Enter the no: '))
+print('Sum:', a+b+c+d+f+g+h+i)
+
+
+# In[ ]:
+
+
+
+
